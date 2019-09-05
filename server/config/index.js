4c47df8c68d82a70edf940eb13d41bd982c15364
huangyanyu <695914800@qq.com>
2019-09-05 15:55:17 +0800

    add server

---
+++ server/config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  clientId: 'efa87842c625b2657dfc', // github的clientId
+  clientSecret: '8db90fa85a8d55ca4d0d78b14e79e05746c2a116', // github的cclientSecret
+  msgKey: 'c32cc2c0ea084c9db36d9243041078f1', // 短信发送的key
+  emailPass: 'dlceqqgjayanbiif', // 发送邮件的pass
+  scope: ['user']
+}
