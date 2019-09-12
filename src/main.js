f0477cbeb4bf5e840a9e76d5ead01e3606375aaf
huangyanyu <695914800@qq.com>
2019-09-12 17:36:57 +0800

    模拟数据mock

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios';
 import http from './http'
+import './mock'
 
 Vue.use(ElementUI);
 Vue.prototype.$axios = http;
